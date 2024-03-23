e0b1492548ac9be2632ecf110adedbc01f5e9b7f
Ahmad Cahyana <yana.santri17@gmail.com>
2024-03-23 23:10:51 +0700

    Session 4

--- src/Lampu.java
+++ src/Lampu.java
@@ -1,6 +1,6 @@
 public class Lampu {
-    boolean menyala = false;
-    String warna = "Putih";
+    private boolean menyala = false;
+    private String warna = "Putih";
     public void Hidupkan() {
         menyala = true;
         System.out.println("Lampu dinyalakan");
@@ -13,24 +13,47 @@ public class Lampu {
         warna = warnaBaru;
         System.out.println("Warna lampu diganti menjadi " + warna);
     }
+
+    int Angka(int angka) {
+        return angka;
+    }
+
+    public boolean Menyala(int Hidupkan) {
+        System.out.println("Lampu menyala : " + menyala);
+        if (Hidupkan == 1) {
+            menyala = true;
+        } else {
+            menyala = false;
+        }
+        return menyala;
+    }
+    
     public static void main(String[] args) throws Exception {
         Lampu lampu = new Lampu();
 
         // Kondisi awal
-        System.out.println(lampu.menyala);
-        System.out.println(lampu.warna);
+        // System.out.println(lampu.menyala);
+        // System.out.println(lampu.warna);
 
-        // Hidupkan lampu
-        lampu.Hidupkan();
-        System.out.println(lampu.menyala);
-
-        // // Matikan lampu
-        // lampu.Matikan();
+        // // Hidupkan lampu
+        // lampu.Hidupkan();
         // System.out.println(lampu.menyala);
 
-        // // Ganti warna lampu
-        // lampu.GantiWarna("Merah");
-        // System.out.println(lampu.warna);
+        // // // // Matikan lampu
+        // // lampu.Matikan();
+        // // System.out.println(lampu.menyala);
+
+        // // // // Ganti warna lampu
+        // // lampu.GantiWarna("Merah");
+        // // System.out.println(lampu.warna);
+
+        // Lampu lampu2 = new Lampu();
+        // System.out.println(lampu2.menyala);
+        // System.out.println(lampu2.warna);
+
+        boolean x = lampu.Menyala(1);
+        System.out.println(x);
+
         
     }
 }
